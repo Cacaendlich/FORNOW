354d8ce95955128adaa4f6c7abe0f79c8537f1b3
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-12-05 16:38:50 -0300

    add main

--- app/src/main/java/com/camilaendlich/fornow/MainActivity.kt
+++ app/src/main/java/com/camilaendlich/fornow/MainActivity.kt
@@ -7,21 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.camilaendlich.fornow.ui.components.ProductSectionLayout
 import com.camilaendlich.fornow.ui.theme.FORNOWTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FORNOWTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainLayout(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +29,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainLayout(modifier: Modifier = Modifier) {
+    ProductSectionLayout()
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = false)
 @Composable
-fun GreetingPreview() {
+fun MainLayoutPreview() {
     FORNOWTheme {
-        Greeting("Android")
+        MainLayout()
     }
 }
